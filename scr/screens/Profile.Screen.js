4c75486fd9d4a94e84a01ae8737b917e4b154b73
NICOLAS DOS SANTOS SANTANA <nicolas.santana20@etec.sp.gov.br>
2025-04-23 21:43:06 -0300

    v2

--- scr/screens/Profile.Screen.js
+++ scr/screens/Profile.Screen.js
@@ -15,9 +15,29 @@ export default function ProfileScreen({ navigation }) {
                 <View>
                     <Button
                     title="Go to Details"
+                    onPress={() => navigartion.navigate('Home')}
                     />
                 </View>
             </View>
        </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e6e6fa', // Cor de fundo da tela
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        backgroundColor: '#dda0dd', // Cor de fundo do container do botao
+        margin: 10,
+        width: windowWidth * 0.5, // 50% da largura da tela
+        borderRadius: 5,
+    },
+});
